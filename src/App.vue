8c7c40b5a7744ba5b42ef9616be210a8c36b60e1
rival fitrah <rivalfitrah9@gmail.com>
2025-09-22 21:27:36 +0700

    ui login

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 </script>
 
 <template>
-  <div>
-    
-  </div>
+  <Login/>
 </template>
 <style scoped>
 </style>
