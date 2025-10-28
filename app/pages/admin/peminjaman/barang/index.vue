2b37d09577ffa7e5c3feec2c0c8a17a927755414
rival fitrah <rivalfitrah9@gmail.com>
2025-10-28 13:09:35 +0700

    commit

--- app/pages/admin/peminjaman/barang/index.vue
+++ app/pages/admin/peminjaman/barang/index.vue
@@ -10,18 +10,18 @@ import UiInfoBox from '~/components/ui/infoBox.vue'
   <UiInfoBox />
 
   <div class="flex flex-col md:flex-row md:items-center gap-2 mb-4">
-    <div class="flex relative w-full">
+    <div class="flex relative w-full mt-5 gap-4">
       <Search class="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
       <input
         type="text"
-        class="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary text-gray-400"
+        class="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary text-gray-400 bg-white"
         placeholder="Search..."
       />
+      <button
+        class="bg-primary text-white px-4 py-2 rounded-md hover:bg-green-700 transition w-full md:w-auto">
+        Tambah
+      </button>
     </div>
-    <button
-      class="bg-primary text-white px-4 py-2 rounded-md hover:bg-green-700 transition w-full md:w-auto">
-      Tambah
-    </button>
   </div>
 
 
