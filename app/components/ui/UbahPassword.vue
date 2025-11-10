6622f0d883ac49b844cb195b912072dd6a065b06
rival fitrah <rivalfitrah9@gmail.com>
2025-11-10 22:23:42 +0700

    update profil admin

---
+++ app/components/ui/UbahPassword.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+</script>
+
+<template>
+      <div class="bg-white rounded-2xl shadow-lg p-8 mt-6">
+    <h3 class="text-xl font-semibold mb-4">Ganti Password</h3>
+
+    <form class="space-y-4">
+      <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1"
+          >Password Lama</label
+        >
+        <input
+          type="password"
+          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
+          placeholder="Masukkan password lama"
+        />
+      </div>
+
+      <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1"
+          >Password Baru</label
+        >
+        <input
+          type="password"
+          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
+          placeholder="Masukkan password baru"
+        />
+      </div>
+
+      <div>
+        <label class="block text-sm font-medium text-gray-700 mb-1"
+          >Konfirmasi Password Baru</label
+        >
+        <input
+          type="password"
+          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
+          placeholder="Konfirmasi password baru"
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="mt-4 px-6 py-3 bg-primary text-white font-semibold rounded-md shadow hover:bg-utama/80 transition"
+      >
+        Simpan Perubahan
+      </button>
+    </form>
+  </div>
+</template>
--- app/pages/admin/profile/index.vue
+++ app/pages/admin/profile/index.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { Calendar, Clock, Shield } from "lucide-vue-next";
+  import UbahPassword from "~/components/ui/UbahPassword.vue";
 import { ref } from "vue";
   import { onMounted } from "vue";
   definePageMeta({
@@ -45,33 +46,27 @@ import { ref } from "vue";
 
   <!-- Informasi Profil -->
   <div class="flex-1 w-full">
-    <div
-      class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4"
-    >
-    <div class="space-y-3">
-        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <span class="text-gray-500 font-medium">Nama</span>
-        <span class="text-gray-800 font-semibold">{{ user?.nama || "Nama Admin" }}</span>
+      <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4">
+        <div class="space-y-3 w-full">
+          <div class="flex items-center gap-4">
+            <div class="w-32 sm:w-36 text-gray-500 font-medium">Nama</div>
+            <div class="text-gray-800 font-semibold">{{ user?.nama || "Nama Admin" }}</div>
+          </div>
+
+          <div class="flex items-center gap-4">
+            <div class="w-32 sm:w-36 text-gray-500 font-medium">Email</div>
+            <div class="text-gray-800 break-words">{{ user?.email || "admin@example.com" }}</div>
+          </div>
+
+          <div class="flex items-center gap-4">
+            <div class="w-32 sm:w-36 text-gray-500 font-medium">NIM / NIP</div>
+            <div class="text-gray-800 font-semibold">{{ user?.nim || user?.nip || '-' }}</div>
+          </div>
+        </div>
       </div>
 
-      <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <span class="text-gray-500 font-medium">Email</span>
-        <span class="text-gray-800">{{ user?.email || "admin@example.com" }}</span>
-      </div>
-
-      <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <span class="text-gray-500 font-medium">NIM / NIP</span>
-        <span class="text-gray-800 font-semibold">1234568</span>
-      </div>
-
-    </div>
-      
-    </div>
-
-    <!-- Divider -->
     <div class="border-t border-gray-200 my-4"></div>
 
-    <!-- Info Tambahan -->
     <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 text-sm text-gray-600">
       <div
         class="bg-gray-50 p-4 rounded-xl shadow-sm flex items-center justify-between"
@@ -95,49 +90,7 @@ import { ref } from "vue";
   </div>
 </div>
 
-  <div class="bg-white rounded-2xl shadow-lg p-8 mt-6">
-    <h3 class="text-xl font-semibold mb-4">Ganti Password</h3>
+<UbahPassword />
 
-    <form class="space-y-4">
-      <div>
-        <label class="block text-sm font-medium text-gray-700 mb-1"
-          >Password Lama</label
-        >
-        <input
-          type="password"
-          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
-          placeholder="Masukkan password lama"
-        />
-      </div>
 
-      <div>
-        <label class="block text-sm font-medium text-gray-700 mb-1"
-          >Password Baru</label
-        >
-        <input
-          type="password"
-          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
-          placeholder="Masukkan password baru"
-        />
-      </div>
-
-      <div>
-        <label class="block text-sm font-medium text-gray-700 mb-1"
-          >Konfirmasi Password Baru</label
-        >
-        <input
-          type="password"
-          class="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-utama focus:border-utama"
-          placeholder="Konfirmasi password baru"
-        />
-      </div>
-
-      <button
-        type="submit"
-        class="mt-4 px-6 py-3 bg-primary text-white font-semibold rounded-md shadow hover:bg-utama/80 transition"
-      >
-        Simpan Perubahan
-      </button>
-    </form>
-  </div>
 </template>
